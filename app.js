e4a638c3a29a83ac8c75ff47601f8f5d92f0cf82
varu1303 <varunrana13@gmail.com>
2019-12-12 01:01:12 +0530

    Setup for heroku

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('client/build'));
 
-app.listen('8000', () => {
+app.listen( process.env.PORT || '8000', () => {
     console.log('Listening at 8000');
 })
